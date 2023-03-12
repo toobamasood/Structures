df486aaf1c28e0041b9c76d7e5c69f2d779546b9
Tooba Masood <toobamasood786@gmail.com>
2023-03-12 23:19:42 +0500

    Second push

--- 1.cpp
+++ 1.cpp
@@ -5,6 +5,6 @@ int main()
 	int a,b,i;
 	cout<<"Enter starting And ending number."<<endl;
 		cout<<"second line ."<<endl;
-        cout<<"second line ."<<endl;
+        cout<<"3rd line ."<<endl;
 	return 0;
 }
